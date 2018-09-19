114991d16e1012cff11d6f20e3c7909dc6b2bc71
Andrew Delicata <adelicata@hotmail.com>
2018-09-19 23:17:56 +0100

    Code Tidy

--- src/app/address-form/address-form.component.ts
+++ src/app/address-form/address-form.component.ts
@@ -1,66 +1,82 @@
 import { Component, forwardRef, OnInit } from '@angular/core';
-import { ControlValueAccessor, NG_VALUE_ACCESSOR, FormGroup, FormControl, Validators } from '@angular/forms';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR, NG_VALIDATORS, FormControl, Validators, AbstractControl, ValidationErrors, FormGroup } from '@angular/forms';
+import { IAddress } from './address.interface';
 
-export interface IAddress {
-  addressLine1: string;
-  addressNumber: string;
-  city: string;
-  country: string;
-  zipcode: string;
-}
+// Optional: Async validation
+// import { NG_ASYNC_VALIDATORS, ControlValueAccessor, NG_VALUE_ACCESSOR, NG_VALIDATORS, FormControl, Validators, AbstractControl, ValidationErrors, FormGroup } from '@angular/forms';
+// import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-address-form',
   templateUrl: './address-form.component.html',
   styleUrls: ['./address-form.component.css'],
-  providers: [{
-    provide: NG_VALUE_ACCESSOR,
-    useExisting: forwardRef(() => AddressFormComponent),
-    multi: true
-  }]
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => AddressFormComponent),
+      multi: true
+    },
+    {
+      provide: NG_VALIDATORS,
+      useExisting: forwardRef(() => AddressFormComponent),
+      multi: true
+    }
+    // Optional: Async Validators:
+    // { provide: NG_ASYNC_VALIDATORS, useExisting: forwardRef(() => AddressFormComponent), multi: true }
+  ]
 })
 export class AddressFormComponent implements OnInit, ControlValueAccessor {
-  public addressForm: FormGroup;
+  public fg: FormGroup;
+  private previousValue: any;
 
-  private formControls: any;
-  private originalValue: any;
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
-    this.formControls = {
-      addressNumber: new FormControl(null, Validators.required),
+    this.fg = new FormGroup({
       addressLine1: new FormControl(null, Validators.required),
-      city: new FormControl(null, Validators.required),
-      zipcode: new FormControl(null, Validators.required),
+      addressLine2: new FormControl(null, Validators.required),
+      town: new FormControl(null, Validators.required),
+      postcode: new FormControl(null, Validators.required),
+      county: new FormControl(null, Validators.required),
       country: new FormControl(null, Validators.required)
-    };
-    this.addressForm = new FormGroup(this.formControls);
-   }
+    });
 
-<<<<<<< HEAD
     this.fg.valueChanges.subscribe(val => {
       this._onChange(val);
     });
   }
-=======
+
+  validate(control: AbstractControl): ValidationErrors | null {
+    if (!this.fg.valid) {
+      return { valid: this.fg.valid };
+    } else {
+      return null;
+    }
+  }
+
+  // Optional: Async validate
+  // validate(c: AbstractControl): Promise<{ [key: string]: any }> | Observable<{ [key: string]: any }> {
+  //   return new Promise(resolve => {
+  //     // For now, just simulate an api call.  Insert proper code here!
+  //     setTimeout(() => {
+  //       if (!this.fg.valid) {
+  //         resolve({ valid: this.fg.valid });
+  //       } else {
+  //         resolve(null);
+  //       }
+  //     }, 100);
+  //   });
+  // }
+
   private _onChange = (_: any) => {};
   private _onTouched = (_: any) => {};
->>>>>>> parent of 97f60b4... Cody tidy
 
-  set value(value: IAddress) {
-    this.addressForm.setValue(value);
-    this._onChange(this.addressForm.value);
-  }
-
-  get value() {
-    return this.addressForm.value;
-  }
-
-  writeValue(value: IAddress): void {
-    if (value && value !== null) {
-      this.addressForm.setValue(value);
-      this.originalValue = value;
+  public writeValue(val: IAddress): void {
+    if (val && val !== null) {
+      this.previousValue = val;
+      this.fg.setValue(val, { emitEvent: false });
+    } else {
+      this.reset();
     }
   }
 
@@ -72,12 +88,22 @@ export class AddressFormComponent implements OnInit, ControlValueAccessor {
     this._onTouched = fn;
   }
 
-  public formUpdate(event: any) {
-    this._onChange(this.addressForm.value);
+  public formChange(event: any) {
+    this._onChange(this.fg.value);
   }
 
   public reset() {
-    this.addressForm.setValue(this.originalValue);
-    this._onChange(this.addressForm.value);
+    if (this.previousValue) {
+      this.fg.patchValue(this.previousValue);
+    } else {
+      this.fg.reset({
+        addressLine1: '',
+        addressLine2: '',
+        postTown: '',
+        postcode: '',
+        county: '',
+        country: ''
+      });
+    }
   }
 }
