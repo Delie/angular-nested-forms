97f60b45ce58bb207387600c265cb6cba57e7be0
Andrew Delicata <adelicata@hotmail.com>
2018-09-19 22:45:22 +0100

    Cody tidy

---
+++ src/app/address-form/address.interface.ts
@@ -0,0 +1,8 @@
+export interface IAddress {
+  addressLine1: string;
+  addressLine2: string;
+  town: string;
+  postcode: string;
+  county: string;
+  country: string;
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,36 +1,51 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormGroup, FormControl, Validators, ValidationErrors, Validator } from '@angular/forms';
+import { AddressFormComponent } from './address-form/address-form.component';
+import { IAddress } from './address-form/address.interface';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+  public fg: FormGroup;
 
-  public registerForm: FormGroup;
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
-    const defaultBillingAddress = {
-      addressNumber: '21',
-      addressLine1: 'address line 1',
-      city: 'city',
-      zipcode: 'zipcode',
-      country: 'country'
+    const billingAddress: IAddress = {
+      addressLine1: '21 Test Street',
+      addressLine2: 'Testerville',
+      town: 'Oxford',
+      postcode: 'OX13 9ZZ',
+      county: 'Oxon',
+      country: 'United Kingdom'
     };
 
-    const defaultShippingAddress = {
-      addressNumber: '15',
-      addressLine1: 'addr  1',
-      city: 'Oxford',
-      zipcode: 'zipcode',
-      country: 'country'
+    const shippingAddress: IAddress = {
+      addressLine1: '14 Tester Avenue',
+      addressLine2: 'Tester Farm',
+      town: 'Swindon',
+      postcode: 'SN4 1AA',
+      county: 'Wiltshire',
+      country: 'United Kingdom'
     };
 
-    this.registerForm = new FormGroup({
-      billingAddress: new FormControl(defaultBillingAddress, Validators.required),
-      shippingAddress: new FormControl(defaultShippingAddress, Validators.required)
+    this.fg = new FormGroup({
+      billingAddress: new FormControl(billingAddress),
+      shippingAddress: new FormControl(shippingAddress)
     });
-   }
+  }
+
+  public submitForm() {
+    alert('submit() called.  insert save logic here');
+  }
+
+  public resetForm() {
+    this.fg.patchValue({
+      billingAddress: null,
+      shippingAddress: null
+    });
+  }
 }
